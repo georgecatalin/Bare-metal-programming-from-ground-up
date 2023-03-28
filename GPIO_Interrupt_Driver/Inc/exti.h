bc9b093a799555937d3b3af4651296798007fbbc
georgecatalin <george.calin@gmail.com>
2023-03-28 13:24:22 +0300

    Test the GPIO Interrupt Driver

--- GPIO_Interrupt_Driver/Inc/exti.h
+++ GPIO_Interrupt_Driver/Inc/exti.h
@@ -11,4 +11,6 @@
 
 #include "stm32f429xx.h"
 
+void pc13_exti_init(void);
+
 #endif /* EXTI_H_ */
--- GPIO_Interrupt_Driver/Src/main.c
+++ GPIO_Interrupt_Driver/Src/main.c
@@ -11,33 +11,69 @@
  */
 
 #include <stdio.h>
+#include "exti.h"
 #include "uart.h"
-#include "timer.h"
 
-#define TIM2_SR_UIF (1UL<<0)  // Bit 0 UIF: Update interrupt flag
+#define EXTI_PR_LINE_13	(1UL<<13)
 
-#define TIM3_SR_CC2IF	(1UL<<2)
+#define GPIOB_ENABLE (1UL<<1)
+#define PIN7	(1UL<<7)
+#define LED_PIN	PIN7
 
-int time_stamp=0;
+static void exti_callback(void);
 
-/* Set up : Connect a jumper from PB7 to PA7 */
+
 int main(void)
 {
-	tim4_output_compare();
-	tim3_input_capture();
+	/* Enable the clock access via AHB1 to GPIO B */
+	RCC->AHB1ENR=GPIOB_ENABLE;
+
+	/* Set the mode in the MODER registry to output for port B7 */
+	GPIOB->MODER &=~(1UL<<15); //'0'
+	GPIOB->MODER |=(1UL<<14); //'1'
+
+
+	pc13_exti_init();
+	uart3_tx_init();
 
 	for(;;)
 	{
-		//Wait until the edge is captured
-		while(!(TIM3->SR & TIM3_SR_CC2IF));
-
-		/* Read value into variable */
-		time_stamp = TIM3->CCR1;
 
 	}
 
 }
 
+/* ***********************************************************************************************************************************
+ * Explanations: At this stage we wish to fire up the interrupt. Hence we need to implement the ISR
+ * (Interrupt Service Routine) function which is also called Interrupt Hander (IH)
+ * The name of the function matters as it has to exist in the vector table recognized by the NVIC hardware
+ * so one gotta check the possibilities of names for the ISR in the Startup folder > startup_stm32f429zitx.s
+ * There is a section that referts to the vector table
+ * line 129 : g_pfnVectors: ....
+ * In our case we check which of the name describes the interrupt service routine we need: EXTI15_10_IRQHandler()
+ * Note: EXTI15_10_IRQHandler() function is servicing all the interrupts coming from Line 10,11,12,13,14,15 hence we need to clarify which
+ * is the line where the interrupts comes from. This is clarified using the RM0090:  Pending register (EXTI_PR) as it says
+ * "PRx: Pending bit -> 0: No trigger request occurred ; 1: selected trigger request occurred "
+ * If that bit is '1' then it means the interrupt came from that line
+ * *************************************************************************************************************************************
+ */
 
+void EXTI15_10_IRQHandler()
+{
+	/* Check if the interrupt originated from Line 13 */
+	if(EXTI->PR & EXTI_PR_LINE_13)
+	{
+		/* Clear the PR Flag */
+		EXTI->PR |=(EXTI_PR_LINE_13);
 
+		exti_callback();
+	}
+}
 
+static void exti_callback(void)
+{
+	printf("Someone pressed that button, alligator...\n\r");
+
+	/* Toggle the user LED */
+	GPIOB->ODR ^=LED_PIN;
+}
