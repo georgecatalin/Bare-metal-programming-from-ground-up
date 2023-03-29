6bda2cd45ea89df687e71b0ee87ed71a24c8d526
georgecatalin <george.calin@gmail.com>
2023-03-29 09:13:06 +0300

    Added the version of ADC Interrupt Driver with the bit of Continuous Conversion enabled

---
+++ ADC_Interrupt_Driver_Version_2/Src/adc.c
@@ -0,0 +1,109 @@
+/*
+ * adc.c
+ *
+ *  Created on: 28 Mar 2023
+ *  Author: George Calin
+ * 	Target Development Board: STM32 Nucleo F429ZI
+ */
+
+#include "adc.h"
+
+#define ADC1EN	(1UL<<8)
+#define GPIOA_ENR	(1UL<<0)
+#define ADC_SQR3	(1UL<<0)
+#define ADC_SQR1_LEN 0x00 // setting the entire registry SQR1 at once to 0 bit by bit
+#define CR2_ADON (1UL<<0)
+#define CR2_SWSTART (1UL<<30)
+#define ADC_CR2CONT (1UL<<1)
+
+
+#define ADCCR1_EOCIE (1UL<<5)
+
+void pa1_adc_init(void)
+{
+	/* *** CONFIGURE THE ADC GPIO PIN *** */
+
+	/* Enable clock access to ADC GPIO pin which is PA1 */
+	RCC->AHB1ENR |= GPIOA_ENR;
+
+	/* Set the mode of PA1 to analog */
+	GPIOA->MODER |= (1UL<<3); // '1'
+	GPIOA->MODER |= (1UL<<2); // '1'
+
+	/* *** CONFIGURE THE ADC Module *** */
+	/* Enable clock access to ADC */
+	RCC->APB2ENR |= ADC1EN;
+
+	/* ** Configure the parameters of the ADC ** */
+
+	/* parameter: Conversion sequence start */
+	ADC1->SQR3 = ADC_SQR3; // we set it to the value so writing all other bits, not only the one on position SQR1 cause we have only one channel in this use case
+
+	/* parameter: Conversion sequence length */
+	ADC1->SQR1 = ADC_SQR1_LEN;
+
+
+	/* Enable ADC module */
+	ADC1->CR2 |= CR2_ADON;
+}
+
+
+void pa1_adc_interrupt_init(void)
+{
+	/* *** CONFIGURE THE ADC GPIO PIN *** */
+
+	/* Enable clock access to ADC GPIO pin which is PA1 */
+	RCC->AHB1ENR |= GPIOA_ENR;
+
+	/* Set the mode of PA1 to analog */
+	GPIOA->MODER |= (1UL<<3); // '1'
+	GPIOA->MODER |= (1UL<<2); // '1'
+
+	/* *** CONFIGURE THE ADC Module *** */
+	/* Enable clock access to ADC */
+	RCC->APB2ENR |= ADC1EN;
+
+	/* ** Configure the parameters of the ADC ** */
+
+	/* Enable ADC end-of-conversion interrupt */
+	/* *************************************************************************************************************************************************************
+	 * Explanations: Info about this is taken from RM0090: ADC control register 1 (ADC_CR1)
+	 * We are interested in bit 5 of this registry which stands for EOCIE Interrupt enable for EOC as it says
+	 * 		This bit is set and cleared by software to enable/disable the end of conversion interrupt. 0: EOC interrupt disabled, 1: EOC interrupt enabled. An interrupt is generated when the EOC bit is set.
+	 * *************************************************************************************************************************************************************
+	 */
+	ADC1->CR1 |= ADCCR1_EOCIE;
+
+	/* Enable ADC interrupt in NVIC */
+	NVIC_EnableIRQ(ADC_IRQn);
+
+	/* parameter: Conversion sequence start */
+	ADC1->SQR3 = ADC_SQR3; // we set it to the value so writing all other bits, not only the one on position SQR1 cause we have only one channel in this use case
+
+	/* parameter: Conversion sequence length */
+	ADC1->SQR1 = ADC_SQR1_LEN;
+
+
+	/* Enable ADC module */
+	ADC1->CR2 |= CR2_ADON;
+
+}
+
+void start_conversion(void)
+{
+	/* Enable continuous conversion */
+	ADC1->CR2 |=ADC_CR2CONT;
+
+	/* Start ADC Conversion */
+	ADC1->CR2 |= CR2_SWSTART;
+
+}
+
+uint32_t adc_read(void)
+{
+	/* Wait for conversion to be completed */
+	while(!(ADC1->SR & SR_EOC));
+
+	/*Read the result of the conversion*/
+	return (ADC1->DR);
+}
---
+++ ADC_Interrupt_Driver_Version_2/Src/main.c
@@ -0,0 +1,73 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : George Calin
+ * @brief          : Main program body
+ * Target board: Nucleo 144 family
+ *
+ * Notes: User LD3: a red user LED is connected to PB14 on the Nucleo 144 family boards
+ * User LD2: a blue user LED is connected to PB7.
+ ******************************************************************************
+ */
+
+#include <stdio.h>
+#include "uart.h"
+#include "adc.h"
+
+uint32_t sensor_val;
+
+static void adc_callback();
+
+int main(void)
+{
+	uart3_tx_init();
+	pa1_adc_interrupt_init();
+	start_conversion();
+
+	while(1)
+	{
+
+	}
+
+}
+
+
+/* ***********************************************************************************************************************************
+ * Explanations: At this stage we wish to fire up the interrupt. Hence we need to implement the ISR
+ * (Interrupt Service Routine) function which is also called Interrupt Hander (IH)
+ * The name of the function matters as it has to exist in the vector table recognized by the NVIC hardware
+ * so one gotta check the possibilities of names for the ISR in the Startup folder > startup_stm32f429zitx.s
+ * There is a section that referts to the vector table
+ * line 129 : g_pfnVectors: ....
+ * In our case we check which of the name describes the interrupt service routine we need: ADC_IRQHandler
+ * *************************************************************************************************************************************
+ */
+
+void ADC_IRQHandler(void)
+{
+	/* Check if the EOC end of conversion was achieved */
+	/* *********************************************************************************************************************************
+	 * Explanations: The info is taken from RM0090: ADC status register (ADC_SR)
+	 * This registry uses only bits 0..5 while the rest of the bits are reserved.
+	 * We are interested in bit 5 EOC Regular channel end of conversion as it says
+	 * 		This bit is set by hardware at the end of the conversion of a regular group of channels.
+	 * 		0: Conversion not complete (EOCS=0), or sequence of conversions not complete (EOCS=1)
+	 * 		1: Conversion complete (EOCS=0), or sequence of conversions complete (EOCS=1)
+	 * *********************************************************************************************************************************
+	 */
+	ADC->CCR |=(1UL<<17);
+	ADC->CCR |=(1UL<<16);
+
+	if(ADC1->SR & SR_EOC)
+	{
+		/* Clear the EOC flag */
+		ADC1->SR &=~ SR_EOC;
+		adc_callback();
+	}
+}
+
+static void adc_callback()
+{
+	sensor_val = ADC1->DR;
+	printf("Sensor value: %d \n\r", (int) sensor_val);
+}
